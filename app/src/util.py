d0708022ea0cef122704113e205b149c2ff37b3f
abemaru <ch4nge.lightening@gmail.com>
2022-09-14 23:37:10 +0900

    小改良

--- app/src/util.py
+++ app/src/util.py
@@ -1,12 +1,13 @@
-'''概要: 出力ファイルのパスを作成するためのモジュール'''
+"""概要: 出力ファイルのパスを作成するためのモジュール"""
+
 
 def make_output_file_path(input_directory, output_directory, file_path):
-    '''与えられたファイルのパスからファイル名だけを抜き取り、名前を変更して出力先のディレクトリへ出力する
+    """与えられたファイルのパスからファイル名だけを抜き取り、名前を変更して出力先のディレクトリへ出力する
     Args:
       input_directory: file_pathのファイル名までの入力パス
       output_directory: file_pathのファイル名までの出力パス
       file_path: 対象ファイルのパス
-    '''
+    """
 
     file_name = file_path.replace(input_directory, '')
     file_name_without_csv = file_name.replace('.csv', '')
